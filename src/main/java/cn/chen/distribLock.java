aea34c852dbf6d0d56201dcc56050309343e53db
pgleo <peng_chen_9012@163.com>
2019-04-17 10:49:55 +0800

    val加上机器ip，防止对机器是出现地址相同的情况

--- src/main/java/cn/chen/distribLock.java
+++ src/main/java/cn/chen/distribLock.java
@@ -1,9 +1,13 @@
 package cn.chen;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.UUID;
 import org.apache.commons.io.FileUtils;
 
 import org.springframework.util.ResourceUtils;
 import org.springframework.util.StringUtils;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPool;
 
 import java.io.File;
@@ -18,6 +22,22 @@ public class distribLock{
 
     private static String unlockScript ;
 
+    private static String localIp ;
+    /**
+     * redis ip
+     */
+    public static String host = "127.0.0.1";
+    /**
+     * redis端口号
+     */
+    public static int port = 6379;
+    {
+        try {
+            localIp = InetAddress.getLocalHost().getAddress().toString();
+        }catch (UnknownHostException e){
+            localIp = UUID.randomUUID().toString();
+        }
+    }
     public static boolean lock(String lockName){
         return lock(lockName,Thread.currentThread(),DEFAULT_EXPIRE_TIME);
     }
@@ -28,15 +48,15 @@ public class distribLock{
 
     public static boolean lock(String lockName,Object object,long expire){
         try {
-            JedisPool pool = new JedisPool("127.0.0.1",6379);
+            JedisPool pool = new JedisPool(host,port);
             Jedis redis = pool.getResource();
             File file = ResourceUtils.getFile("classpath:script/lock.lua");
             if(StringUtils.isEmpty(lockScript)) {
                 lockScript = new String(FileUtils.readFileToByteArray(file));
             }
             while (true) {
-                Object rs = redis.eval(lockScript, 1, lockName,object.toString(), expire + "");
-                if((Long)rs==1) break;
+                Object rs = redis.eval(lockScript, 1, lockName,localIp+object.toString(), expire + "");
+                if((Long)rs==1) {break;}
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
